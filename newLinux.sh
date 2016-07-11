e832cf27b4875303150db441d65d07c0ce48f654
kodama09 <kodamacolson@gmail.com>
2016-07-11 11:37:57 +0200

    First things first
    Shell commands to install all the package you need to start a good Linux

---
+++ newLinux.sh
@@ -0,0 +1,41 @@
+echo "Ce fichier va installer l'ensemble des paquets utiles à un bon fonctionnement et un bon paramétrage de votre Linux.";
+echo "réalisé par kodama v.03.20160701";
+echo "";
+
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer ;
+sudo apt-get update ;
+sudo apt-get install grub-customizer ;
+
+sudo apt-get install vim ;
+sudo apt-get install vim-gtk-py2;
+
+sudo apt-get install git ;
+
+sudo apt-get install python-pip ;
+sudo apt-get install python-pip3 ;
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose ;
+sudo apt-get install python-matplotlib ;
+sudo apt-get install python-tk ;
+sudo apt-get install python3-numpy ;
+sudo apt-get install python3-matplotlib ;
+sudo pip3 install matplotlib ;
+sudo apt-get install python-keybinder ;
+
+
+sudo apt-get install latex-full
+
+sudo apt-get install cmake
+
+sudo apt-get install g++
+
+sudo apt-get install libglew-dev
+
+sudo apt-get install freeglut3-dev
+
+sudo apt-get install build-essential checkinstall
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+
+sudo apt-get install qt-sdk ;
+
+sudo apt-get install figlet ;
+sudo apt-get install cmatrix
